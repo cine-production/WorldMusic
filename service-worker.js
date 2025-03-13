221bb212004661fa593e33da3691d57f53b84816
cineprod <142513195+cine-production@users.noreply.github.com>
2025-03-14 00:55:14 +0100

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,12 +1,15 @@
+// Version du cache
 const CACHE_NAME = 'World Music';
 const urlsToCache = [
-    '/',
-    '/index.html',
-    'https://fonts.googleapis.com/css?family=Roboto',
-    'https://code.jquery.com/jquery-3.6.0.min.js'
+    '/', // Page principale
+    '/index.html', // Page principale (redondance pour certains navigateurs)
+    '/offline.html', // Page hors connexion
+    '/WorldMusic.png', // Icône/logo
+    'https://fonts.googleapis.com/css?family=Roboto', // Police Roboto
+    'https://code.jquery.com/jquery-3.6.0.min.js' // jQuery
 ];
 
-// Installation du Service Worker
+// Installation du Service Worker et mise en cache des fichiers
 self.addEventListener('install', function(event) {
     event.waitUntil(
         caches.open(CACHE_NAME)
@@ -17,7 +20,7 @@ self.addEventListener('install', function(event) {
     );
 });
 
-// Activation du Service Worker
+// Activation du Service Worker et nettoyage des anciens caches
 self.addEventListener('activate', function(event) {
     event.waitUntil(
         caches.keys().then(function(cacheNames) {
@@ -35,12 +38,26 @@ self.addEventListener('activate', function(event) {
 // Interception des requêtes
 self.addEventListener('fetch', function(event) {
     event.respondWith(
+        // Vérifier d'abord dans le cache
         caches.match(event.request)
             .then(function(response) {
                 if (response) {
-                    return response;
+                    return response; // Servir depuis le cache si disponible
                 }
-                return fetch(event.request);
+
+                // Si pas dans le cache, tenter une requête réseau
+                return fetch(event.request)
+                    .then(function(networkResponse) {
+                        // Si la requête réseau réussit, mettre à jour le cache
+                        return caches.open(CACHE_NAME).then(function(cache) {
+                            cache.put(event.request, networkResponse.clone());
+                            return networkResponse;
+                        });
+                    })
+                    .catch(function() {
+                        // Si la requête réseau échoue (hors connexion), afficher la page hors connexion
+                        return caches.match('/offline.html');
+                    });
             })
     );
 });
